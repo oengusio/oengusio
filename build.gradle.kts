0bafeb82d084924882994ae92e3503c5624b5130
duncte123 <contact@duncte123.me>
2020-12-10 09:33:42 +0100

    Change plugin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     application
 
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("org.springframework.boot") version "2.4.0"
 }
 
@@ -22,14 +21,6 @@ java {
 application {
     mainClassName = "${project.group}.OengusApplication"
 }
-
-dependencyManagement {
-    val springCloudVersion = "Greenwich.RELEASE"
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
-    }
-}
-
 
 repositories {
     jcenter()
