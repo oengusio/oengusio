b688030889b926f1368ec6db967e92679b2af6c5
duncte123 <contact@duncte123.me>
2021-01-12 09:49:05 +0100

    Clear marathons on sandbox

---
+++ src/main/java/app/oengus/service/ClearSandboxService.java
@@ -0,0 +1,43 @@
+package app.oengus.service;
+
+import app.oengus.dao.MarathonRepository;
+import app.oengus.dao.UserRepository;
+import javassist.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("sandbox")
+public class ClearSandboxService {
+    private static final Logger LOG = LoggerFactory.getLogger(ClearSandboxService.class);
+
+    @Autowired
+    private MarathonRepository marathonRepository;
+
+    @Autowired
+    private MarathonService marathonService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void purgeEntries() {
+        LOG.info("Deleting marathons");
+
+        this.marathonRepository.findAll().forEach((marathon) -> {
+            try {
+                this.marathonService.delete(marathon.getId());
+            } catch (NotFoundException e) {
+                LOG.error("Failed to delete marathon " + marathon.getId(), e);
+            }
+        });
+
+        LOG.info("Deleting users");
+
+        this.userRepository.deleteAll();
+    }
+}
