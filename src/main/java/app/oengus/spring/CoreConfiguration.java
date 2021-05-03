1db13e55a410840beb6796392ea3dd038e87d5d1
duncte123 <contact@duncte123.me>
2021-05-03 08:23:54 +0200

    Fix username limit
    Closes #47

--- src/main/java/app/oengus/spring/CoreConfiguration.java
+++ src/main/java/app/oengus/spring/CoreConfiguration.java
@@ -3,8 +3,6 @@ package app.oengus.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.PathProvider;
 import springfox.documentation.builders.PathSelectors;
@@ -18,19 +16,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 public class CoreConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
             // override the default base path to make sure it displays properly on the docs page
             .pathProvider(new PathProvider() {
+                // this was removed in version 2.10, wtf
                 @Override
                 public String getApplicationBasePath() {
                     return "/api/";
