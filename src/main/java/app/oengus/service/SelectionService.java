80b7705c06ac44e5df039ab64b95c500149157bb
duncte123 <contact@duncte123.me>
2021-05-05 14:25:03 +0200

    spaces > tabs

--- src/main/java/app/oengus/service/SelectionService.java
+++ src/main/java/app/oengus/service/SelectionService.java
@@ -22,87 +22,87 @@ import java.util.stream.Collectors;
 @Service
 public class SelectionService {
 
-	@Autowired
-	private SelectionRepositoryService selectionRepository;
+    @Autowired
+    private SelectionRepositoryService selectionRepository;
 
-	@Autowired
-	private CategoryRepositoryService categoryRepositoryService;
+    @Autowired
+    private CategoryRepositoryService categoryRepositoryService;
 
     @Autowired
     private MarathonRepositoryService marathonRepositoryService;
 
-	public Map<Integer, SelectionDto> findByMarathon(final String marathonId) {
-		final Marathon marathon = new Marathon();
-		marathon.setId(marathonId);
+    public Map<Integer, SelectionDto> findByMarathon(final String marathonId) {
+        final Marathon marathon = new Marathon();
+        marathon.setId(marathonId);
 
-		final List<Selection> selections = this.selectionRepository.findByMarathon(marathon);
+        final List<Selection> selections = this.selectionRepository.findByMarathon(marathon);
 
-		return this.modelToDtos(selections);
-	}
+        return this.modelToDtos(selections);
+    }
 
     public List<Selection> findByMarathon(final Marathon marathon) {
         return this.selectionRepository.findByMarathon(marathon);
     }
 
-	public Map<Integer, SelectionDto> findByMarathon(final String marathonId, final List<Status> statuses) {
-		final Marathon marathon = new Marathon();
-		marathon.setId(marathonId);
-		if (statuses == null || statuses.isEmpty()) {
-			return this.findByMarathon(marathonId);
-		} else {
-			return this.modelToDtos(this.selectionRepository.findByMarathonAndStatusIn(marathon, statuses));
-		}
-	}
+    public Map<Integer, SelectionDto> findByMarathon(final String marathonId, final List<Status> statuses) {
+        final Marathon marathon = new Marathon();
+        marathon.setId(marathonId);
+        if (statuses == null || statuses.isEmpty()) {
+            return this.findByMarathon(marathonId);
+        } else {
+            return this.modelToDtos(this.selectionRepository.findByMarathonAndStatusIn(marathon, statuses));
+        }
+    }
 
-	public Map<Integer, SelectionDto> findAllByCategory(final List<Category> categories) {
-		final List<Selection> selections = this.selectionRepository.findAllByCategory(categories);
-		return this.modelToDtos(selections);
-	}
+    public Map<Integer, SelectionDto> findAllByCategory(final List<Category> categories) {
+        final List<Selection> selections = this.selectionRepository.findAllByCategory(categories);
+        return this.modelToDtos(selections);
+    }
 
-	private Map<Integer, SelectionDto> modelToDtos(final List<Selection> selections) {
-		final Map<Integer, SelectionDto> dtos = new HashMap<>();
-		selections.forEach(selection -> {
-			final SelectionDto dto = new SelectionDto();
-			dto.setId(selection.getId());
-			dto.setCategoryId(selection.getCategory().getId());
-			dto.setStatus(selection.getStatus());
-			dtos.put(selection.getCategory().getId(), dto);
-		});
+    private Map<Integer, SelectionDto> modelToDtos(final List<Selection> selections) {
+        final Map<Integer, SelectionDto> dtos = new HashMap<>();
+        selections.forEach(selection -> {
+            final SelectionDto dto = new SelectionDto();
+            dto.setId(selection.getId());
+            dto.setCategoryId(selection.getCategory().getId());
+            dto.setStatus(selection.getStatus());
+            dtos.put(selection.getCategory().getId(), dto);
+        });
 
-		return dtos;
-	}
+        return dtos;
+    }
 
-	@Transactional
-	public void saveOrUpdate(final String marathonId, final List<SelectionDto> dtos) throws NotFoundException {
-		final Marathon marathon = this.marathonRepositoryService.findById(marathonId);
-		final List<Selection> newSelections = new ArrayList<>();
-		final Iterable<Category> categories = this.categoryRepositoryService.findAllById(
-		    dtos.stream().map(SelectionDto::getCategoryId).collect(Collectors.toList())
+    @Transactional
+    public void saveOrUpdate(final String marathonId, final List<SelectionDto> dtos) throws NotFoundException {
+        final Marathon marathon = this.marathonRepositoryService.findById(marathonId);
+        final List<Selection> newSelections = new ArrayList<>();
+        final Iterable<Category> categories = this.categoryRepositoryService.findAllById(
+            dtos.stream().map(SelectionDto::getCategoryId).collect(Collectors.toList())
         );
 
-		final Map<Integer, Category> categoryMap = new HashMap<>();
-		categories.forEach((category) -> categoryMap.put(category.getId(), category));
+        final Map<Integer, Category> categoryMap = new HashMap<>();
+        categories.forEach((category) -> categoryMap.put(category.getId(), category));
 
-		dtos.forEach((dto) -> {
-			final Category category = categoryMap.get(dto.getCategoryId());
+        dtos.forEach((dto) -> {
+            final Category category = categoryMap.get(dto.getCategoryId());
 
-			if (category != null) {
-				final Selection selection = new Selection();
-				selection.setId(dto.getId());
-				selection.setCategory(category);
-				selection.setMarathon(marathon);
-				selection.setStatus(dto.getStatus());
+            if (category != null) {
+                final Selection selection = new Selection();
+                selection.setId(dto.getId());
+                selection.setCategory(category);
+                selection.setMarathon(marathon);
+                selection.setStatus(dto.getStatus());
                 newSelections.add(selection);
-			}
-		});
+            }
+        });
 
         this.selectionRepository.saveAll(newSelections);
 
-	}
+    }
 
-	@Transactional
-	public void rejectTodos(final Marathon marathon) {
-		this.selectionRepository.rejectTodos(marathon);
-	}
+    @Transactional
+    public void rejectTodos(final Marathon marathon) {
+        this.selectionRepository.rejectTodos(marathon);
+    }
 
 }
