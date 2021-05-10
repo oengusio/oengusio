7c1e2fcf407ac2a40f5b5aa138535c68800f7711
duncte123 <contact@duncte123.me>
2021-05-10 13:32:21 +0200

    Update spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     java
     application
 
-    id("org.springframework.boot") version "2.4.2"
+    id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2020.0.1")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2020.0.2")
     }
 }
 
