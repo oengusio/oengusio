e752a0c5fa8bb53a3cfa85721a29e4a6085c45cd
duncte123 <contact@duncte123.me>
2021-07-19 16:01:01 +0200

    Add getter to fetch applications for marathons

--- src/main/java/app/oengus/service/ApplicationRepositoryService.java
+++ src/main/java/app/oengus/service/ApplicationRepositoryService.java
@@ -3,12 +3,17 @@ package app.oengus.service;
 import app.oengus.dao.ApplicationRepository;
 import app.oengus.dao.ApplicationUserInformationRepository;
 import app.oengus.entity.dto.ApplicationUserInformationDto;
+import app.oengus.entity.model.Application;
 import app.oengus.entity.model.ApplicationUserInformation;
+import app.oengus.entity.model.Marathon;
 import app.oengus.entity.model.User;
 import app.oengus.helper.BeanHelper;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ApplicationRepositoryService {
@@ -41,5 +46,10 @@ public class ApplicationRepositoryService {
         BeanHelper.copyProperties(dto, infoForUser);
 
         return this.applicationUserInformationRepository.save(infoForUser);
+    }
+
+
+    public List<Application> getApplications(Marathon marathon) {
+        return new ArrayList<>();
     }
 }
