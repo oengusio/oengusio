728b6a9a6df840a9ac347d53cb0ce4792e32bf87
duncte123 <contact@duncte123.me>
2021-07-22 11:42:22 +0200

    Format

--- src/main/java/app/oengus/entity/model/Availability.java
+++ src/main/java/app/oengus/entity/model/Availability.java
@@ -3,6 +3,7 @@ package app.oengus.entity.model;
 import app.oengus.spring.model.Views;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.Cacheable;
@@ -10,7 +11,6 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import java.time.ZonedDateTime;
-import org.hibernate.annotations.Cache;
 
 @Embeddable
 @Cacheable
@@ -19,28 +19,28 @@ import org.hibernate.annotations.Cache;
 public class Availability {
 
     @NotNull
-	@Column(name = "date_from")
-	@JsonView(Views.Public.class)
-	private ZonedDateTime from;
+    @Column(name = "date_from")
+    @JsonView(Views.Public.class)
+    private ZonedDateTime from;
 
     @NotNull
-	@Column(name = "date_to")
-	@JsonView(Views.Public.class)
-	private ZonedDateTime to;
+    @Column(name = "date_to")
+    @JsonView(Views.Public.class)
+    private ZonedDateTime to;
 
-	public ZonedDateTime getFrom() {
-		return this.from;
-	}
+    public ZonedDateTime getFrom() {
+        return this.from;
+    }
 
-	public void setFrom(final ZonedDateTime from) {
-		this.from = from;
-	}
+    public void setFrom(final ZonedDateTime from) {
+        this.from = from;
+    }
 
-	public ZonedDateTime getTo() {
-		return this.to;
-	}
+    public ZonedDateTime getTo() {
+        return this.to;
+    }
 
-	public void setTo(final ZonedDateTime to) {
-		this.to = to;
-	}
+    public void setTo(final ZonedDateTime to) {
+        this.to = to;
+    }
 }
