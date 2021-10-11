1ed22fc7d55c63b1a1064b33df712903866393ec
duncte123 <contact@duncte123.me>
2021-10-11 22:08:54 +0200

    Remove join

--- src/main/java/app/oengus/entity/model/Marathon.java
+++ src/main/java/app/oengus/entity/model/Marathon.java
@@ -35,7 +35,7 @@ public class Marathon {
     @Pattern(regexp = "^[\\w\\- ]{4,40}$")
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JsonView(Views.Public.class)
     @JoinColumn(name = "creator_id")
     @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
