4370491a5e12cb2a782f89c07a52b5da5d696fc6
duncte123 <contact@duncte123.me>
2024-07-22 14:36:54 +0200

    Document question model

--- src/main/java/app/oengus/adapter/rest/dto/v2/marathon/request/QuestionsUpdateRequest.java
+++ src/main/java/app/oengus/adapter/rest/dto/v2/marathon/request/QuestionsUpdateRequest.java
@@ -1,13 +1,19 @@
 package app.oengus.adapter.rest.dto.v2.marathon.request;
 
 import app.oengus.adapter.rest.dto.v2.marathon.QuestionDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @Setter
+@Schema(description = "Update questions for a marathon")
 public class QuestionsUpdateRequest {
+    // TODO: limit the amount of questions that can be added
+    @NotNull
+    @Schema(description = "The questions to be updated, only questions of type SUBMISSION are supported", required = true)
     private List<QuestionDto> questions;
 }
