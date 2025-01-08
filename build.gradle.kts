daffd63bdb16e3c472ca5e6ab718bf344fe24d8c
duncte123 <contact@duncte123.me>
2025-01-08 18:34:19 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 
 project.group = "app.oengus"
 // Version code is year.month.(release num)
-project.version = "2025.01.1"
+project.version = "2025.01.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
