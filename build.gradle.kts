c405629a3457f4488cdc8ecc36ac064a610a5b5d
duncte123 <contact@duncte123.me>
2025-09-13 09:07:23 +0200

    Remove asciidoctor plugin (we don't need it)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.asciidoctor.jvm.convert") version "4.0.5" // TODO: do we need this?
     id("io.freefair.lombok") version "8.14.2"
 }
 
@@ -177,11 +176,6 @@ tasks.test {
         showStandardStreams = System.getenv("CI") == null
         exceptionFormat = TestExceptionFormat.FULL
     }
-}
-
-tasks.asciidoctor {
-    inputs.dir(snippetsDir)
-    dependsOn(tasks.test)
 }
 
 compileJava.apply {
